c532135fb6f6cd6fc39bf5d5250b81b524306137
Domingo Suarez Torres <domingo.suarez@gmail.com>
2015-05-31 02:25:47 -0500

    initial implementation, going to bed now.

---
+++ src/main/java/com/domingosuarez/boot/actuate/health/RabbitMQHealthIndicator.java
@@ -0,0 +1,60 @@
+package com.domingosuarez.boot.actuate.health;
+
+import com.netflix.hystrix.HystrixCommand;
+import com.netflix.hystrix.HystrixCommandGroupKey;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.boot.actuate.health.AbstractHealthIndicator;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.Status;
+import org.springframework.util.Assert;
+
+import static java.util.stream.Collectors.toMap;
+
+/**
+ * Simple implementation of a {@link org.springframework.boot.actuate.health.HealthIndicator} returning status information for the
+ * RabbitMQ messaging system.
+ * <p>
+ * The Health check is performed using a Hystrix command.
+ *
+ * @author Domingo Suarez Torres
+ * @since 0.1.0
+ */
+public class RabbitMQHealthIndicator extends AbstractHealthIndicator {
+  private static final Status RABBIT_DOWN = new Status("RABBIT_DOWN");
+  private final RabbitTemplate rabbitTemplate;
+
+  public RabbitMQHealthIndicator(RabbitTemplate rabbitTemplate) {
+    Assert.notNull(rabbitTemplate, "RabbitTemplate must not be null.");
+    this.rabbitTemplate = rabbitTemplate;
+  }
+
+  @Override
+  protected void doHealthCheck(Health.Builder builder) throws Exception {
+    new HealthIndicatorCommand(rabbitTemplate, builder).execute();
+  }
+
+  static class HealthIndicatorCommand extends HystrixCommand<Health.Builder> {
+    private RabbitTemplate rabbitTemplate;
+    private Health.Builder builder;
+
+    public HealthIndicatorCommand(RabbitTemplate rabbitTemplate, Health.Builder builder) {
+      super(HystrixCommandGroupKey.Factory.asKey("ExampleGroup"));
+      this.builder = builder;
+      this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @Override
+    protected Health.Builder run() throws Exception {
+      return builder.up()
+        .withDetail("server_properties", rabbitTemplate.execute(channel ->
+          channel.getConnection().getServerProperties().entrySet().stream()
+            .collect(toMap(entry -> entry.getKey(), entr -> entr.getValue().toString()))));
+    }
+
+    @Override
+    protected Health.Builder getFallback() {
+      return builder.status(RABBIT_DOWN)
+        .withException((Exception) getFailedExecutionException());
+    }
+  }
+}
