52b609846f0a1d193ddf0645ce6ec2907474127f
Domingo Suarez Torres <domingo.suarez@gmail.com>
2015-05-31 10:47:41 -0500

    added plugins for oss release, added License file, fixed license header

--- src/main/java/com/domingosuarez/boot/actuate/health/RabbitMQHealthIndicator.java
+++ src/main/java/com/domingosuarez/boot/actuate/health/RabbitMQHealthIndicator.java
@@ -1,11 +1,12 @@
-/*
- * Copyright 2012-2014 the original author or authors.
+/**
+ *
+ * Copyright (C) 2014-2015 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,7 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.domingosuarez.boot.actuate.health;
 
 import com.netflix.hystrix.HystrixCommand;
@@ -25,7 +25,6 @@ import org.springframework.boot.actuate.health.Status;
 import org.springframework.util.Assert;
 
 import java.util.AbstractMap.SimpleEntry;
-import java.util.function.Function;
 
 import static java.util.stream.Collectors.toMap;
 
