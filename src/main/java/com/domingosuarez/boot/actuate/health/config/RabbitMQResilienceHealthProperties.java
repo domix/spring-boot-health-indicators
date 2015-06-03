6151c1b04d92716835e66c1c146e856860c3d7f5
Domingo Suarez Torres <domingo.suarez@gmail.com>
2015-06-03 16:00:49 -0500

    Documenting the settings for RabbitMQ health indicator

--- src/main/java/com/domingosuarez/boot/actuate/health/config/RabbitMQResilienceHealthProperties.java
+++ src/main/java/com/domingosuarez/boot/actuate/health/config/RabbitMQResilienceHealthProperties.java
@@ -21,26 +21,60 @@ import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 /**
+ * Configuration properties for the Hystrix command & the health indicator.
+ *
  * @author Domingo Suarez Torres
  * @since 0.1.6
  */
 @ConfigurationProperties(prefix = "resilience.health.rabbitmq")
 public class RabbitMQResilienceHealthProperties {
+
+  /**
+   * The Hystrix CommandGroupKey.
+   * Default to 'resilience.health'.
+   */
   @Setter
   @Getter
   private String hystrixCommandGroupKey = "resilience.health";
+
+  /**
+   * The Hystrix ThreadPoolKey.
+   * Default to 'HealthIndicators'.
+   */
   @Setter
   @Getter
   private String hystrixThreadPoolKey = "HealthIndicators";
+
+  /**
+   * The Hystrix CommandKey.
+   * Default to 'rabbitHealth'.
+   */
   @Setter
   @Getter
   private String hystrixCommandKey = "rabbitHealth";
+
+  /**
+   * The Hystrix executionTimeoutInMilliseconds.
+   * Default to '1000'.
+   */
   @Setter
   @Getter
   private Integer executionIsolationThreadTimeoutInMilliseconds = 1000; // default => executionTimeoutInMilliseconds: 1000 = 1 second;
+
+  /**
+   * When RabbitMQ is Up and running, the health indicator will include all the server properties by default.
+   * Setting this flag to false will only include the server version.
+   */
   @Setter
   @Getter
   private Boolean includeServerProperties = Boolean.TRUE;
+
+  /**
+   * When RabbitMQ is down, the default response by the indicator will be 'RABBIT_DOWN'
+   * and the Application health will not be affected by the failing RabbitMQ.
+   * Setting this flag to true will behave like the default one, this means the response
+   * will be 'DOWN' and the Applicacion health will be 'DOWN' as well.
+   */
   @Setter
   @Getter
   private Boolean useClassicDown = Boolean.FALSE;
